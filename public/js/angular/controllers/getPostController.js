65bd8f426663999bd6df96eb76b8e45741fe9e10
Salvador Villegas <salvador_villegas@bitbucket.org>
2016-04-28 15:37:55 -0500

    Angular error parse

--- public/js/angular/controllers/getPostController.js
+++ public/js/angular/controllers/getPostController.js
@@ -1,4 +1,5 @@
 app.controller('getPostController', ['$scope', '$http', '$stateParams', '$state', function($scope, $http, $stateParams, $state){
+	$scope.posts = [];
 	if($stateParams.postId > 0) {
 		$http.get('/api/v1/posts/'+$stateParams.postId)
 	        .success(function(data, status, headers, config) {
@@ -14,10 +15,16 @@ app.controller('getPostController', ['$scope', '$http', '$stateParams', '$state'
 	$scope.save = function(){
 		if(typeof $stateParams.postId == "undefined"){
 			$http.post('/api/v1/posts',$scope.post)
-	            .success(function(data, status, headers, config) {
+	            .then(function(data, status, headers, config) {
 	            	$scope.posts.push(data);
 	            	$state.go('index');
-	            });
+	            },
+	            function(response) {
+	            	$scope.errorMessageShow = true;
+	            	$scope.errorMessage = parseErrors(response.data.errors)
+	            	console.log(response.data.errors);
+	            }
+	            );
 		} else {
 			$http.put('/api/v1/posts/'+$stateParams.postId,$scope.post)
 	            .success(function(data, status, headers, config) {
@@ -34,3 +41,13 @@ app.controller('getPostController', ['$scope', '$http', '$stateParams', '$state'
         });
 	}
 }]);
+
+function parseErrors(errors)
+{
+	var messages = '';
+	$.each(errors, function(key, message){
+        messages += key+": "+message;
+    });
+    message=messages;
+    return message;
+}
